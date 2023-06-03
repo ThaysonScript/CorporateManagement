6ef1890ee5a5671f61e7d648eb81402d9975b7a6
ThaysonScript <thaysonguedes01@gmail.com>
2023-06-03 00:02:09 -0300

    deletando models defeituosos

--- app/Models/Site/Site.php
+++ app/Models/Site/Site.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Site;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+class Site extends Model
 {
     use HasFactory;
 }
