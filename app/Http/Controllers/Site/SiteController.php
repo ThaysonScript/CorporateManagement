fdea8265955cf8636314ea8e50d29e57bc9fc534
ThaysonScript <thaysonguedes01@gmail.com>
2023-06-03 23:29:23 -0300

    concerto - pegando categoriaEstoque pelo usuario logado

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use App\Models\CadastroEstoques\CategoriaEstoque;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SiteController extends Controller
 {
@@ -15,7 +16,9 @@ class SiteController extends Controller
 
     public function PaginaHome()
     {
-        $estoques = CategoriaEstoque::all();
+        $estoques = CategoriaEstoque::all()->where('user_id', $user_id = Auth::user()->id);
+
+        // $estoques = $estoques->all()->where('user_id', $user_id = Auth::user()->id);
 
         return view('site.home', compact('estoques'));
     }
