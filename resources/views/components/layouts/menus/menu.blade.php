65cf6de8c393b692b3ba542dd7c057a2a7481cf6
ThaysonScript <thaysonguedes01@gmail.com>
2023-07-09 00:30:34 -0300

    fix - resolvido menu

--- resources/views/components/layouts/menus/menu.blade.php
+++ resources/views/components/layouts/menus/menu.blade.php
@@ -1,9 +1,9 @@
 <div>
-  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</button>
+  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Menu de Opcoes</button>
   
   <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Menu de Opcoes</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
