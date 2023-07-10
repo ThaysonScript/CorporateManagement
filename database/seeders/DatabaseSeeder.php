2a494150a71ac2e9057c7e39085595899f1e5ff4
ThaysonScript <thaysonguedes01@gmail.com>
2023-07-09 23:41:15 -0300

    comment - seed produtoseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        ProdutoSeed::class;
+        // ProdutoSeed::class;
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
